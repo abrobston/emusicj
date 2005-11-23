623c93800b718471aef77b340a2e24f659f4c54b
robin <Unknown>
2005-11-23 08:19:15 +0000

    Version bump

--- src/nz/net/kallisti/emusicj/Constants.java
+++ src/nz/net/kallisti/emusicj/Constants.java
@@ -9,7 +9,7 @@ package nz.net.kallisti.emusicj;
  */
 public class Constants {
 
-    public final static String VERSION = "0.10";
+    public final static String VERSION = "0.11";
     
     public final static String APPNAME = "eMusic/J";
 
