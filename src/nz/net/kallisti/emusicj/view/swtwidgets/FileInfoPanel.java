df72c20091a953cd8007bf60586c9b8f7c537630
robin <Unknown>
2006-06-21 13:06:54 +0000

    Added copyright information to the source files, and set the build so that it will put the COPYING and README files into the dist

--- src/nz/net/kallisti/emusicj/view/swtwidgets/FileInfoPanel.java
+++ src/nz/net/kallisti/emusicj/view/swtwidgets/FileInfoPanel.java
@@ -1,3 +1,24 @@
+/*
+    eMusic/J - a Free software download manager for emusic.com
+    http://www.kallisti.net.nz/emusicj
+    
+    Copyright (C) 2005, 2006 Robin Sheat
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ */
 package nz.net.kallisti.emusicj.view.swtwidgets;
 
 import java.io.File;
@@ -22,7 +43,7 @@ import org.eclipse.swt.widgets.Label;
  * IDownloader instance). Once it is in place, a call to {@link setDownloader}
  * defines what to display. It keeps an image cache for album covers.
  * 
- * $Id: FileInfoPanel.java 101 2006-01-10 11:07:33Z robin $
+ * $Id$
  *
  * @author robin
  */
--- src/nz/net/kallisti/emusicj/view/swtwidgets/StatusLine.java
+++ src/nz/net/kallisti/emusicj/view/swtwidgets/StatusLine.java
@@ -1,3 +1,24 @@
+/*
+    eMusic/J - a Free software download manager for emusic.com
+    http://www.kallisti.net.nz/emusicj
+    
+    Copyright (C) 2005, 2006 Robin Sheat
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ */
 package nz.net.kallisti.emusicj.view.swtwidgets;
 
 import org.eclipse.swt.SWT;
@@ -9,7 +30,7 @@ import org.eclipse.swt.widgets.Label;
  * can be put somewhere. Done in a class on it's own to allow increasing its
  * functionality should that been needed.</p>
  * 
- * <p>$Id:$</p>
+ * <p>$Id$</p>
  *
  * @author robin
  */
