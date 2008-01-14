0235c34e0b29420eebc70b448f7f52eb5f5be172
robin <Unknown>
2008-01-14 10:50:03 +0000

    Fix infinite loop when the artist/album/track name contains a '%' wildcard

--- src/nz/net/kallisti/emusicj/controller/Preferences.java
+++ src/nz/net/kallisti/emusicj/controller/Preferences.java
@@ -207,7 +207,7 @@ public abstract class Preferences implements IPreferences {
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
 			if (c < ' ' || c == '/' || c == '\\' || c > '~' || c == ':'
-					|| c == '*' || c == '?' || c == '"' || c == '&')
+					|| c == '*' || c == '?' || c == '"' || c == '&' || c == '%')
 				str.setCharAt(i, '_');
 		}
 	}
