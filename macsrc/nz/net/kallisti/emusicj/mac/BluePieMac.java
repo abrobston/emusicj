af3c6454ecae661d0ebaead85d08b6aad466af40
robin <Unknown>
2008-07-04 12:47:19 +0000

    Mac loader for blue pie

---
+++ macsrc/nz/net/kallisti/emusicj/mac/BluePieMac.java
@@ -0,0 +1,35 @@
+package nz.net.kallisti.emusicj.mac;
+
+import nz.net.kallisti.emusicj.ReggaeCountry;
+import nz.net.kallisti.emusicj.controller.IEMusicController;
+import nz.net.kallisti.emusicj.mac.OpenDocHandler;
+
+/**
+ * <p>
+ * This is a mac loader for the ReggaeCountry varient of the application. The
+ * purpose of this loader is to hook in the mac open document handler to the
+ * program. This should be the entry point when running on OSX
+ * </p>
+ * 
+ * $Id$
+ * 
+ * @author robin
+ */
+public class BluePieMac extends ReggaeCountry {
+
+	public BluePieMac(String[] args) {
+		super(args);
+	}
+
+	@Override
+	public void startApp(IEMusicController controller, String[] args) {
+		// put the opendoc hooks in
+		new OpenDocHandler(controller);
+		super.startApp(controller, args);
+	}
+
+	public static void main(String[] args) {
+		new BluePieMac(args);
+	}
+
+}
