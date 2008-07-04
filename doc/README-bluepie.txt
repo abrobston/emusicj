32529aebee8b76aeab0ca32e724e19f763ee97a4
robin <Unknown>
2008-07-04 14:09:44 +0000

    Basics for bluepie in place, still need real images

---
+++ doc/README-bluepie.txt
@@ -0,0 +1,9 @@
+Blue Pie Download Manager is for the Blue Pie 
+(http://www.bluepie.org) downloading service.
+
+This program was written by and is copyright Robin Sheat <robin@kallisti.net.nz>.
+It may be freely distributed under the terms of the GNU GPL. 
+
+Artwork copyright is owned by bluepie.org and Blue Pie Productions
+
+-- Robin Sheat, 05/07/2008
--- src/nz/net/kallisti/emusicj/controller/BluePiePreferences.java
+++ src/nz/net/kallisti/emusicj/controller/BluePiePreferences.java
@@ -3,6 +3,8 @@ package nz.net.kallisti.emusicj.controller;
 import nz.net.kallisti.emusicj.misc.files.IFileNameCleaner;
 import nz.net.kallisti.emusicj.strings.IStrings;
 
+import com.google.inject.Inject;
+
 /**
  * <p>
  * This contains the preference overrides specific to the Blue Pie variant of
@@ -15,6 +17,7 @@ import nz.net.kallisti.emusicj.strings.IStrings;
  */
 public class BluePiePreferences extends EmusicjPreferences {
 
+	@Inject
 	public BluePiePreferences(IStrings strings, IFileNameCleaner nameCleaner) {
 		super(strings, nameCleaner);
 	}
