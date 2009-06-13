2f2120ca62f0d8b0fc9e971e7731c3840a5cf903
Robin Sheat <robin@kallisti.net.nz>
2009-06-13 16:56:26 +1200

    Improved the image changing with synchronise and so on

--- src/nz/net/kallisti/emusicj/view/swtwidgets/graphics/URLDynamicImageProvider.java
+++ src/nz/net/kallisti/emusicj/view/swtwidgets/graphics/URLDynamicImageProvider.java
@@ -6,7 +6,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,6 +43,13 @@ public class URLDynamicImageProvider implements IDynamicImageProvider,
 	private Display display;
 	private Image image;
 	private File cacheDir;
+	private final Object lock = new Object();
+	/**
+	 * This tracks URLs that have already been downloaded this session so that
+	 * multiple requests aren't made.
+	 */
+	private final Set<URL> requested = Collections
+			.synchronizedSet(new HashSet<URL>());
 
 	/**
 	 * <p>
@@ -73,50 +82,59 @@ public class URLDynamicImageProvider implements IDynamicImageProvider,
 	 * @param url
 	 *            the URL to source the image from
 	 */
-	private void downloadImage(URL url) {
-		// If the URL is null, then we can't download anything. Really, we
-		// shouldn't be getting one in this case, but it happens.
-		if (url == null)
-			return;
-		// First thing, check cache - files in the cache are known by an MD5 of
-		// their URL.
-		MessageDigest md5;
-		try {
-			md5 = MessageDigest.getInstance("MD5");
-			md5.update(url.toString().getBytes());
-			Base64 base64 = new Base64();
-			String filename = new String(base64.encode(md5.digest()));
-			File cacheFile = new File(this.cacheDir, filename);
-			if (cacheFile.exists()) {
-				setImage(cacheFile);
-			}
-			// Download the file to the cache
-			ISimpleDownloader downloader = downloaderProvider.get();
-			downloader.setURL(url);
-			downloader.setOutputFile(cacheFile);
-			downloader.addListener(new ISimpleDownloadListener() {
-				public void downloadFailed(ISimpleDownloader downloader) {
+	private void downloadImage(final URL url) {
+		synchronized (lock) {
+			// If the URL is null, then we can't download anything. Really, we
+			// shouldn't be getting one in this case, but it happens.
+			if (url == null)
+				return;
+			// First thing, check cache - files in the cache are known by an MD5
+			// of
+			// their URL.
+			MessageDigest md5;
+			try {
+				md5 = MessageDigest.getInstance("MD5");
+				md5.update(url.toString().getBytes());
+				Base64 base64 = new Base64();
+				String filename = new String(base64.encode(md5.digest()));
+				File cacheFile = new File(this.cacheDir, filename);
+				if (cacheFile.exists()) {
+					setImage(cacheFile);
 				}
+				if (requested.contains(url))
+					return;
+				// Download the file to the cache
+				ISimpleDownloader downloader = downloaderProvider.get();
+				downloader.setURL(url);
+				downloader.setOutputFile(cacheFile);
+				downloader.addListener(new ISimpleDownloadListener() {
+					public void downloadFailed(ISimpleDownloader downloader) {
+					}
 
-				public void downloadSucceeded(ISimpleDownloader downloader,
-						File file) {
-					setImage(file);
-				}
-			});
-			downloader.start();
-		} catch (NoSuchAlgorithmException e) {
-			logger
-					.log(
-							Level.SEVERE,
-							"Unable to use cache to save images - missing hashing algorithm",
-							e);
+					public void downloadSucceeded(ISimpleDownloader downloader,
+							File file) {
+						requested.add(url);
+						setImage(file);
+					}
+				});
+				downloader.start();
+			} catch (NoSuchAlgorithmException e) {
+				logger
+						.log(
+								Level.SEVERE,
+								"Unable to use cache to save images - missing hashing algorithm",
+								e);
+			}
 		}
 	}
 
 	public void changeURL(URL url) {
-		if (cacheDir == null)
-			throw new IllegalArgumentException("setParams must be called first");
-		downloadImage(url);
+		synchronized (lock) {
+			if (cacheDir == null)
+				throw new IllegalArgumentException(
+						"setParams must be called first");
+			downloadImage(url);
+		}
 	}
 
 	/**
