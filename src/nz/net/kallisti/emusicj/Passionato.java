ba8541ed4ec76c6dc7113827b7850b8c77fd5790
Robin Sheat <robin@kallisti.net.nz>
2009-09-03 18:58:14 +1200

    Basic build setup for Passionato

---
+++ src/nz/net/kallisti/emusicj/Passionato.java
@@ -0,0 +1,80 @@
+/*
+    eMusic/J - a Free software download manager for emusic.com
+    http://www.kallisti.net.nz/emusicj
+    
+    Copyright (C) 2005, 2006 Robin Sheat
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ */
+package nz.net.kallisti.emusicj;
+
+import nz.net.kallisti.emusicj.bindings.Bindings;
+import nz.net.kallisti.emusicj.bindings.PassionatoBindings;
+import nz.net.kallisti.emusicj.controller.IEmusicjController;
+import nz.net.kallisti.emusicj.view.IEmusicjView;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Stage;
+
+/**
+ * <p>
+ * This is the main class for the Passionato downloader. It doesn't do a whole
+ * lot except for start the other parts of the system going. This involves
+ * creating an instance of the controller, and giving it a view to use.
+ * </p>
+ * 
+ * <p>
+ * $Id$
+ * </p>
+ * 
+ * @author robin
+ */
+public class Passionato {
+
+	/**
+	 * Initialises the components of the system.
+	 * 
+	 * @param args
+	 *            command line parameters.
+	 */
+	public Passionato(String[] args) {
+		Injector injector = Guice.createInjector(Stage.PRODUCTION,
+				new Bindings(), new PassionatoBindings());
+		IEmusicjView view = injector.getInstance(IEmusicjView.class);
+		view.setState(IEmusicjView.ViewState.STARTUP);
+		IEmusicjController controller = injector
+				.getInstance(IEmusicjController.class);
+		controller.run(args);
+	}
+
+	/**
+	 * This may be overridden to provide custom starters for other platforms
+	 * 
+	 * @param controller
+	 *            the application controller
+	 * @param args
+	 *            the command line arguments
+	 */
+	public void startApp(IEmusicjController controller, String[] args) {
+		controller.run(args);
+	}
+
+	public static void main(String[] args) {
+		new Passionato(args);
+	}
+
+}
---
+++ src/nz/net/kallisti/emusicj/view/images/passionato/PassionatoImageFactory.java
@@ -0,0 +1,103 @@
+package nz.net.kallisti.emusicj.view.images.passionato;
+
+import nz.net.kallisti.emusicj.view.SWTView;
+import nz.net.kallisti.emusicj.view.images.AbstractImageFactory;
+import nz.net.kallisti.emusicj.view.images.IImageFactory;
+import nz.net.kallisti.emusicj.view.swtwidgets.graphics.IDynamicImageProvider;
+import nz.net.kallisti.emusicj.view.swtwidgets.graphics.IStreamAndURLDynamicImageProvider;
+import nz.net.kallisti.emusicj.view.swtwidgets.graphics.IStreamDynamicImageProvider;
+import nz.net.kallisti.emusicj.view.swtwidgets.graphics.IURLDynamicImageProvider;
+
+import org.eclipse.swt.graphics.Image;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+/**
+ * <p>
+ * This image factory supplies the images that are used for eMusic/J
+ * </p>
+ * 
+ * $Id:$
+ * 
+ * @author robin
+ */
+public class PassionatoImageFactory extends AbstractImageFactory implements
+		IImageFactory {
+
+	private IStreamDynamicImageProvider appIconProvider;
+
+	@Inject
+	public PassionatoImageFactory(
+			Provider<IStreamDynamicImageProvider> streamImageProvider,
+			Provider<IURLDynamicImageProvider> urlImageProvider,
+			Provider<IStreamAndURLDynamicImageProvider> streamAndUrlImageProvider) {
+		super(streamImageProvider, urlImageProvider, streamAndUrlImageProvider);
+	}
+
+	public Image getDownloadingIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"emusicj-dl-16.png"));
+	}
+
+	public Image getNotDownloadingIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"emusicj-nodl-16.png"));
+	}
+
+	public Image getCancelIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"cancel.png"));
+	}
+
+	public Image getPauseIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"pause.png"));
+	}
+
+	public Image getRequeueIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"requeue.png"));
+	}
+
+	public Image getStartIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"start.png"));
+	}
+
+	public Image getStartSmallIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"start_small.png"));
+	}
+
+	public Image[] getAppIcons() {
+		return new Image[] {
+				new Image(display, this.getClass().getResourceAsStream(
+						"emusicj-app-16.png")),
+				new Image(display, this.getClass().getResourceAsStream(
+						"emusicj-app-32.png")),
+				new Image(display, this.getClass().getResourceAsStream(
+						"emusicj-app-48.png")),
+				new Image(display, this.getClass().getResourceAsStream(
+						"emusicj-app-64.png")),
+				new Image(display, this.getClass().getResourceAsStream(
+						"emusicj-app-128.png")) };
+	}
+
+	public Image getAboutLogo() {
+		return new Image(SWTView.getDisplay(), this.getClass()
+				.getResourceAsStream("emusicj-about.png"));
+	}
+
+	public IDynamicImageProvider getApplicationLogoProvider() {
+		appIconProvider = initStreamImageProvider(appIconProvider, this
+				.getClass().getResourceAsStream("passionato-logo.png"));
+		return appIconProvider;
+	}
+
+	public Image getFolderIcon() {
+		return new Image(display, this.getClass().getResourceAsStream(
+				"folder_32.png"));
+	}
+
+}
